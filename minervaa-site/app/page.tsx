0564c8f1750dfc8274b2c60942c59221985a01d5
jarjunarnold <jarjunarnold@gmail.com>
2026-04-23 15:14:54 -0400

    Update page.tsx

--- minervaa-site/app/page.tsx
+++ minervaa-site/app/page.tsx
@@ -315,7 +315,7 @@ export default function HomePage() {
             <p>Jothi Nagar, Pollachi - 642001.</p>
             <p>admin@minervaaschool.com</p>
             <p><strong>Phone:</strong> +91 96004 17171</p>
-            <p><strong>     :</strong> +91 99449 72100</p>
+            <p>     : +91 99449 72100</p>
             <p><strong>     :</strong> +91 99949 59484</p>
           </div>
         </div>
