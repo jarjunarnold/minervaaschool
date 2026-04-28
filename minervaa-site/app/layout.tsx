2405fc46f05b7fd72799c33d83a46c0e7cfa375a
jarjunarnold <jarjunarnold@gmail.com>
2026-04-27 20:20:13 -0400

    Update layout.tsx

--- minervaa-site/app/layout.tsx
+++ minervaa-site/app/layout.tsx
@@ -4,7 +4,13 @@ import type { Metadata } from 'next';
 export const metadata: Metadata = {
   title: 'Minervaa Vidhya Mandhir',
   description:
-    'Rooted in Values. Driven by Excellence. A trusted matriculation school in Pollachi delivering structured academic excellence through a CBSE-oriented curriculum approach.'
+    'Rooted in Values. Driven by Excellence. A trusted matriculation school in Pollachi delivering structured academic excellence.',
+  
+  icons: {
+    icon: '/images/logo.png',
+    shortcut: '/images/logo.png',
+    apple: '/images/logo.png',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
